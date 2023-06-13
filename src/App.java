fc483ed8fdca188b1bbd166a98685dbb5239aea5
eduardorittner <eduardorittnerc@gmail.com>
2023-06-12 22:30:04 -0300

    Inicial

--- src/App.java
+++ src/App.java
@@ -1,29 +1,21 @@
-import java.util.Scanner;
+import javax.swing.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("O que você quer fazer?");
-        System.out.println("Montar uma refeição: r");
-        System.out.println("Cadastrar um novo ingrediente: i");
+    public static void main(String args[]) {
 
-        Scanner scanner = new Scanner(System.in);
-        String entrada = scanner.nextLine();
-        if (entrada.equals("i")) {
-            // Para adicionar um novo ingrediente, recebe o nome e calorias por grama e cria
-            // uma nova instância do objeto ingrediente, e depois adiciona ele no arquivo
-            // calcount/data/ingredientes.txt
+        // Criando o Frame
+        JFrame frame = new JFrame("Testando");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
 
-            System.out.println("Qual o nome do ingrediente?");
-            String nome = scanner.nextLine();
-            System.out.println("Quantas calorias por grama ele tem?");
-            int calorias = Integer.parseInt(scanner.nextLine());
-            Ingrediente novoIngrediente = new Ingrediente(nome, calorias);
-            System.out.println("Novo ingrediente adicionado:");
-            System.out.println("Nome: " + novoIngrediente.getNome());
-            System.out.println("Calorias por grama " + novoIngrediente.getCalorias());
-            // FileManager.criarArquivo("ingredientes.txt", "porra nenhuma");
-            FileManager.AdicionarIngrediente(novoIngrediente);
-        }
-        scanner.close();
+        // Criando a barra de navegação
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuUsuario = new JMenu("Usuário");
+        JMenu menuIngredientes = new JMenu("Ingredientes");
+        JMenu menuPratos = new JMenu("Pratos");
+
+        JButton button = new JButton("Clique!");
+        frame.getContentPane().add(button);
+        frame.setVisible(true);
     }
 }
