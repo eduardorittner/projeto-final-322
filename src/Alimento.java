fdcb419eb8fb77010e36e32aa4688c9e18e16f5a
Vinicius Leme <viniciuslemesoares@gmail.com>
2023-06-14 16:03:50 -0300

    Inicio de Alimento e Refeição

--- src/Alimento.java
+++ src/Alimento.java
@@ -1,3 +1,96 @@
+import java.util.*;
+
+
 public abstract class Alimento {
+
+    public final int id;        // id final para que possamos referenciar o alimento em uma lista
+    public String nome;
+    public double prot;
+    public double fat;
+    public double carb;
+    public double cal;
+
+    private static ArrayList<Integer> listaIds = new ArrayList<Integer>();
+    Random rand = new Random();
+
+    /* O alimento não tem um peso específico pois todos eles serão registrados
+    considerando 1g, assim seus macros sempre estarão relacionados a 1g do alimento.
+    No construtor coloquei a variável peso para o caso de a entrada não ser 1g*/
+
+    // Constructor 
+
+    public Alimento(String nome, double prot, double fat, double carb, double cal, double peso) {
+        this.nome = nome;
+        if (peso != 1.0) {
+            prot = prot/peso;
+            fat = fat/peso;
+            carb = carb/peso;
+            cal = cal/peso;
+        }
+        this.nome = nome; // TODO validação do nome (somente letras)
+        this.fat = fat; 
+        this.prot = prot;
+        this.carb = carb;
+        this.cal = cal;
+        int temp = rand.nextInt(1000000);   // Gerador de id único por alimento
+        while (listaIds.contains(temp)) {
+            temp = rand.nextInt(1000000);
+        }
+        this.id = temp;
+    }
+
+    // Getters e setters
     
-}
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getProt() {
+        return prot;
+    }
+
+    public void setProt(double prot) {
+        this.prot = prot;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCarb() {
+        return carb;
+    }
+
+    public void setCarb(double carb) {
+        this.carb = carb;
+    }
+
+    public double getCal() {
+        return cal;
+    }
+
+    public void setCal(double cal) {
+        this.cal = cal;
+    }
+
+    @Override
+    public String toString() {
+        return nome + "\nCarbs:" + carb + "; Prot:" + prot + "; Fat:" + fat + "; Cal:" + cal + "\n";
+    }
+
+    void calcularMacros() {};
+
+
+}   
---
+++ src/Ingrediente.java
@@ -0,0 +1,37 @@
+//import java.util.*;
+
+
+public class Ingrediente extends Alimento{
+
+    public int porcao; // Seria o tamanho de uma porção "normal" em gramas
+    //Até onde pensei o constructor vai ser o mesmo, assim como as variáveis de classe
+
+    // Constructor
+
+    public Ingrediente(String nome, double prot, double fat, double carb, double cal, double peso, int porcao) {
+        super(nome, prot, fat, carb, cal, peso);
+        this.porcao = porcao;
+    }
+
+    // Getter e Setter
+
+    public int getPorcao() {
+        return porcao;
+    }
+
+    public void setPorcao(int porcao) {
+        this.porcao = porcao;
+    }
+    
+
+    @Override
+    public String toString() {
+        return nome + " - Porção de " + porcao + "g\nCarbs:" + carb + "; Prot:" + prot + "; Fat:" + fat + "; Cal:" + cal + "\n";
+    }
+
+    @Override
+    public void calcularMacros() {
+
+    }
+
+}
