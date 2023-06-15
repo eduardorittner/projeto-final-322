aa3d6329064c911eab7e0943b9d5dcb3ff1b8cfa
Vinicius Leme <viniciuslemesoares@gmail.com>
2023-06-15 14:31:08 -0300

    Primeira versão Receita, Ingredientes e Alimento

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
-import javax.swing.*;
+//import javax.swing.*;
 
 public class App {
-        public static void main(String args[]) {
+        public static void main(String args[]) throws Exception {
 
-                // Criando o Frame
+                /*// Criando o Frame
                 JFrame frame = new JFrame("Testando");
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.setSize(500, 500);
@@ -18,9 +18,22 @@ public class App {
                 frame.getContentPane().add(button);
                 frame.setVisible(true);
 
-                // sou muito inteligente
+                // sou muito inteligente*/
 
-                
-        
+                Ingrediente maca = new Ingrediente("Maçã", 0, 10, 10, 60, 100);
+                Ingrediente banana = new Ingrediente("Banana", 0, 0, 30, 120, 100);
+                Ingrediente frango = new Ingrediente("Frango", 30, 0, 0, 120, 100);
+                //System.out.println(maca);
+                //System.out.println(banana);
+                //System.out.println(frango);
+                Receita receita1 = Receita.novaReceita("Receita 1");
+                //Receita receita2 = Receita.novaReceita("Receita 2");
+                receita1.adicionarIngrediente(frango, 200);
+                receita1.adicionarIngrediente(banana, 100);
+                receita1.adicionarIngrediente(maca, 100);
+                receita1.imprimirAlimentos();
+                System.out.println(receita1);
+
+
         }
 }
--- src/Ingrediente.java
+++ src/Ingrediente.java
@@ -1,37 +1,46 @@
 //import java.util.*;
 
 
-public class Ingrediente extends Alimento{
+public class Ingrediente extends Alimento implements Cloneable{
 
-    public int porcao; // Seria o tamanho de uma porção "normal" em gramas
-    //Até onde pensei o constructor vai ser o mesmo, assim como as variáveis de classe
+    private double porcao; // a porção será usada na receita e na refeição
+
+    // Seria o tamanho de uma porção "normal" em gramas
+    // Até onde pensei o constructor vai ser o mesmo, assim como as variáveis de class
+    // Deixei a variável porção para alterar quando colocar dentro de uma receita.
+    // A princípio não vai alterar os macros, esses continuam sendo referentes a 1g do alimento
 
     // Constructor
 
-    public Ingrediente(String nome, double prot, double fat, double carb, double cal, double peso, int porcao) {
-        super(nome, prot, fat, carb, cal, peso);
+    public Ingrediente(String nome, double prot, double fat, double carb, double cal, double porcao) {
+        super(nome, prot, fat, carb, cal, porcao);
         this.porcao = porcao;
     }
 
-    // Getter e Setter
+    // Getters e setters
 
-    public int getPorcao() {
+    public double getPorcao() {
         return porcao;
     }
 
-    public void setPorcao(int porcao) {
+    public void setPorcao(double porcao) {
         this.porcao = porcao;
     }
-    
 
     @Override
     public String toString() {
-        return nome + " - Porção de " + porcao + "g\nCarbs:" + carb + "; Prot:" + prot + "; Fat:" + fat + "; Cal:" + cal + "\n";
+        return super.getNome() + " - Porção de " + porcao + "g\n" + super.getMacros();
     }
 
     @Override
-    public void calcularMacros() {
-
+    public Ingrediente clone() throws CloneNotSupportedException {
+        return (Ingrediente) super.clone();
     }
 
+    // Métodos 
+    
+    public Macros CalcularMacrosPorcao() {
+        Macros novo = Macros.macrosPorPorcao(this.getMacros(), porcao);
+        return novo;
+    }   
 }
