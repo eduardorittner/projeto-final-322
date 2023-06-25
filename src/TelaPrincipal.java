d115eed014358361a55dc061096af9f0f9ddb3ba
João Takaki <joaotakaki1@gmail.com>
2023-06-25 20:08:28 -0300

    circulo de macros adicionado

--- src/TelaPrincipal.java
+++ src/TelaPrincipal.java
@@ -1,19 +1,65 @@
-import java.awt.GridLayout;
+import java.awt.*;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class TelaPrincipal extends JFrame {
     public TelaPrincipal(){
         setTitle("Tela Principal");
-        setSize(1280, 720);
+        setSize(390, 844);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setLayout(new GridLayout(FRAMEBITS, ERROR, ALLBITS, ABORT));
+        // setLayout(new GridLayout(FRAMEBITS, ERROR, ALLBITS, ABORT));
+        JPanel painelPrincipal = new JPanel();
+        painelPrincipal.setLayout(new BoxLayout(painelPrincipal, BoxLayout.Y_AXIS));
+        // painelPrincipal.add(Box.createVerticalGlue());
         JMenuBar menuBar = new JMenuBar();
         JMenu usuario = new JMenu("Usuário");
         JMenu historico = new JMenu("Histórico");
         menuBar.add(usuario);
         menuBar.add(historico);
         setJMenuBar(menuBar);
+        //Passar os macros como o formato de string (MUDAR COM .FORMAT OU .TOSTRING)
+        JPanel macros = new JPanel();
+        macros.setLayout(new FlowLayout(FlowLayout.CENTER, 20, 0));
+
+        //Painel de proteinas
+        JPanel proteina = new JPanel();
+        proteina.setLayout(new BoxLayout(proteina, BoxLayout.Y_AXIS));
+        PainelCircular proteinas = new PainelCircular("100 g");
+        JLabel pro = new JLabel("Proteínas");
+        pro.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pro.setBorder(new EmptyBorder(0, 5, 0, 0));
+        proteina.add(proteinas);
+        proteina.add(pro);
+        //Painel de carboidratos
+        JPanel carboidratos = new JPanel();
+        carboidratos.setLayout(new BoxLayout(carboidratos, BoxLayout.Y_AXIS));
+        PainelCircular carbo = new PainelCircular("200 g");
+        JLabel carb = new JLabel("Carboidratos");
+        carb.setAlignmentX(Component.CENTER_ALIGNMENT);
+        carb.setBorder(new EmptyBorder(0, 5, 0, 0));
+        carboidratos.add(carbo);
+        carboidratos.add(carb);
+        //Painel de gorduras
+        JPanel gordura = new JPanel();
+        gordura.setLayout(new BoxLayout(gordura, BoxLayout.Y_AXIS));
+        PainelCircular gorduras = new PainelCircular("300 g");
+        JLabel gor = new JLabel("Gorduras");
+        gor.setAlignmentX(Component.CENTER_ALIGNMENT);
+        gor.setBorder(new EmptyBorder(0, 5, 0, 0));
+        gordura.add(gorduras);
+        gordura.add(gor);
+        JPanel painelHistorioc = new JPanel();
+        painelHistorioc.setBorder(2,2);
+        //Adicionando na Panel geral dos macros
+        macros.add(proteina);
+        macros.add(carboidratos);
+        macros.add(gordura);
+        
+        painelPrincipal.add(macros);
+        add(painelPrincipal);
+
     }
 }
